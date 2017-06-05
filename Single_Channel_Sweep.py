4e546bda08036e81e2c2c0ff9c67948b9d36d2b5
mdlucas <mdlucas@iastate.edu>
2017-06-05 15:59:31 -0500

    channel sweep working

--- Single_Channel_Sweep.py
+++ Single_Channel_Sweep.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+import argparse
 import serial
 import time
 
@@ -5,26 +8,35 @@ import time
 # Argument Parsing
 parser = argparse.ArgumentParser(description='Move motors to n discrete points on an LAPPD channel to take data')
 parser.add_argument('n', type=int, help='The number of samples to be taken on this channel')
+parser.add_argument('-i', '--init', action='store_true', help='Initialize the IPC controller')
 args = parser.parse_args()
 
 # Initialize the motors
-ser = serial.Serial('/dev/ttyUSB0', baudrate=19200)
-ser.write('jmp 16')
-time.sleep(15)
+ser = serial.Serial('/dev/ttyUSB0', baudrate=19200, xonxoff=True)
+if(args.init):
+    print('Initializing')
+    ser.write(b'jmp \n')
+    time.sleep(15)
 
 # Zero the motors
 # TODO
 
 # Move the motors
+ser.write(b'mc 0 0\n')
+ser.write(b'mc 1 0\n')
 
-plate_width = 500000 # Motor encoder ticks #TODO
-step = plate_width / args.n
 
-for i in range(args.n + 1):
-    ser.write('mr 0 {}'.format(step).encode())
+plate_width = 544990 # Motor encoder ticks #TODO
+step = int(plate_width / args.n)
+for i in range(args.n):
+    print('Stop: {}'.format(i))
+    ser.write('mr 0 {}\n'.format(step).encode())
 
     #Take Data #TODO
     time.sleep(5)
 
 print('Finished')
-ser.close()    
+ser.write(b'ma 0 0\n')
+ser.write(b'ma 1 0\n')
+
+ser.close()
