9a6d0733058967dc0b8effd1e35f8c1aed82500d
Miles Lucas <mdlucas@iastate.edu>
2018-04-05 14:45:22 -0500

    added context manager for motors

--- Motors.py
+++ Motors.py
@@ -1,27 +1,28 @@
 import serial
 import time
 
+class MotorConnection():
+    def __enter__(self):
+        ser = serial.Serial('/dev/ttyUSB0', baudrate=19200, xonxoff=False)
+        self.motor = Motors(ser)
+        self.motor.connect()
+        return self.motor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.motor.allstop()
+        self.motor.ser.close()
+
 
 class Motors():
-    def __init__(self, dev='/dev/ttyUSB0', baudrate=19200, xonxoff=True):
-        self.ser = serial.Serial(dev, baudrate=baudrate, xonxoff=xonxoff)
+    def __init__(self, ser):
+        self.ser = ser
+    
+    def connect(self):
         print('Connecting...', end=' ')
         self.ser.write(b'jmp 16\n')
         time.sleep(15)
         print('connected')
     
-    def __del__(self):
-        self.allstop()
-        self.ser.close()
-
-    @classmethod
-    def __enter__(cls):
-        return cls()
-
-    @classmethod
-    def __exit__(cls)
-        del cls
-
     def allstop(self):
         self.stop(0)
         self.stop(1)
@@ -43,17 +44,14 @@ class Motors():
         self.ser.write('mr {} {}\n'.format(motor, distance).encode())
 
 if __name__=='__main__':
-    with Motors() as m:
-        try:
-            while True:
-                cmd = input('@ ')
-                if cmd=='left':
-                    m.move(0, -10000)
-                elif cmd=='right':
-                    m.move(0, 10000)
-                elif cmd=='stop':
-                    m.stop(0)
-                else:
-                    print('Invalid input')
-        except KeyboardInterrupt:
-            m.allstop()
+    with MotorConnection() as m:
+        while True:
+            cmd = input('@ ')
+            if cmd=='left':
+                m.move(0, -10000)
+            elif cmd=='right':
+                m.move(0, 10000)
+            elif cmd=='stop':
+                m.stop(0)
+            else:
+                print('Invalid input')
