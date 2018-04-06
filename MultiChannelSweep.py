659ab3b896f91889de39e1fe1c31dbe1efdadfd8
LAPPD Testbench <lappd@iastate.edu>
2018-04-06 14:01:42 -0500

    quality increase in multichannel

--- MultiChannelSweep.py
+++ MultiChannelSweep.py
@@ -1,34 +1,46 @@
 from Motors import MotorConnection
-from numpy import linspace
+from numpy import linspace, int32
 import time
 import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
 
-def MultiChannelSweep(stops, channels, x0, y0, x1, y1):
+def MultiChannelSweep(stops, channels, xlims, ylims, takeData=False):
     '''
     MultiChannelSweep
     ------------------
 
     '''
-    xstops = linspace(x0, x1, stops)
-    ystops = linspace(y0, y1, channels)
+    xstops = linspace(*xlims, stops, dtype=int32)
+    ystops = linspace(*ylims, channels, dtype=int32)
     with MotorConnection() as m:
-        for x in xstops:
-            m.moveto(0, x)
-            time.sleep(1)
-            for y in ystops:
-                m.moveto(1, y)
-                time.sleep(1)
-    
+        logging.info('Moving to start position')
+        m.moveto(0, xstops[0])
+        m.moveto(1, ystops[0])
+        time.sleep(15)
+        for i, x in enumerate(xstops):
+            yseq = ystops if not i % 2 else reversed(ystops)
+            for j, y in enumerate(yseq):
+                if j == 0:
+                    logging.info('Moving x stage to {}'.format(x))
+                    m.moveto(0, x)
+                else:
+                    logging.info('Moving y stage to {}'.format(y))
+                    m.moveto(1, y)
+                time.sleep(3)
+                m.allstop()
+                if takeData:
+                    pass
+
 
 if __name__=='__main__':
-    # Argument Parsing
     parser = argparse.ArgumentParser(
         description='Move motors to n discrete points across c channels on an LAPPD channel to take data')
     parser.add_argument(
         'n', type=int, help='The number of samples to be taken on a channel')
-    parser.add_argument('c',help='The number of channels to sample from' )
+    parser.add_argument('c', type=int, help='The number of channels to sample from' )
     parser.add_argument('-d', '--data', default=False, action='store_true',
                         help='Take data using the Tek oscilliscope')
     args = parser.parse_args()
-    MultiChannelSweep(args.n, args.c, -100000, -100000, 10000, 10000)
-
+    logging.info('Parsed arguments: {}'.format(args))
+    MultiChannelSweep(args.n, args.c, (-100000, 10000), (-10000, 10000), args.data)
