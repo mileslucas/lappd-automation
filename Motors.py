6d23c204cbda1691c5fc679426de75b3ed7a9c8b
Miles Lucas <mdlucas@iastate.edu>
2018-05-01 15:26:44 -0500

    added documenation to the motors commands

--- Motors.py
+++ Motors.py
@@ -2,6 +2,13 @@ import serial
 import time
 
 class MotorConnection():
+    '''
+    This class provides a connection wrapper to enable safe use of the Motors class
+
+    To use this class, use
+    >>> with MotorConnection as m:
+    >>>     # Whatever motor instructions
+    '''
     def __enter__(self):
         ser = serial.Serial('/dev/ttyUSB0', baudrate=19200, xonxoff=False)
         self.motor = Motors(ser)
@@ -14,10 +21,17 @@ class MotorConnection():
 
 
 class Motors():
+    '''
+    This class wraps all the serial commands for the LAPPD motors. It should not be directly instantiated.
+    Instead a MotorConnection should be established using the with clause
+    '''
     def __init__(self, ser):
         self.ser = ser
     
     def connect(self):
+        '''
+        Connects to the serial port given and issues jmp 16 every time
+        '''
         try:
             self.ser.open()
         except serial.SerialException:
@@ -28,26 +42,71 @@ class Motors():
         print('connected')
 
     def disconnect(self):
+        '''
+        Disconnect the serial connection
+        '''
         self.ser.close()    
 
     def allstop(self):
+        '''
+        Stop all motor motion
+        '''
         self.stop(0)
         self.stop(1)
 
     def park(self):
+        '''
+        Move both motors to (0, 0)
+        '''
         self.reset(0)
         self.reset(1)
         
     def reset(self, motor):
+        '''
+        Moves the given motor to its 0 position
+
+        Params
+        ------
+        motor (int):
+            0 or 1 for the x or y stage motor
+        '''
         self.move(motor, 0)
 
     def moveto(self, motor, position):
+        '''
+        Moves the given motor to an absolute position
+
+        Params
+        ------
+        motor (int):
+            0 or 1 for the x or y stage motor
+        position (int):
+            the absolute motor position
+        '''
         self.ser.write('ma {} {}\n'.format(motor, position).encode())
     
     def stop(self, motor):
+        '''
+        Stops the motion of the given motor
+
+        Params
+        ------
+        motor (int):
+            0 or 1 for the x or y stage motor
+        '''
         self.move(motor, 0)
 
     def move(self, motor, distance):
+        '''
+        Moves the given motor to a relative position
+
+        Params
+        ------
+        motor (int):
+            0 or 1 for the x or y stage motor
+        position (int):
+            the relative motor position
+        '''
         self.ser.write('mr {} {}\n'.format(motor, distance).encode())
 
 if __name__=='__main__':
